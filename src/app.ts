36c1137cbd61769a19f54bb91a126e2342e6af64
Riadh Chougui <riadh@codinglab.com>
2022-01-31 22:30:43 +0100

    feat(backend): add cors middleware for testing

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
-import express from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import { photosRouter } from './modules/photos/photo.route';
 
 const app: express.Application = express();
 
 const port = 3000;
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  next();
+});
 app.use(photosRouter);
-
 app.listen(port, function () {
   console.log(`API is listening on port ${port} !`);
 });
